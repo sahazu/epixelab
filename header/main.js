51e8094b3c3d04ebfd0648706ce1911cd6f6d2db
Zafty <mchibisovm@gmail.com>
2022-10-28 19:59:42 +0300

    yeees

---
+++ header/main.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector('.menuBurger__container'),
+    header = document.querySelector('.menuBurger'),
+    body = document.querySelector('body');
+
+function menuBurgerClick(event) {
+    header.classList.toggle('menuBurger__open');
+    body.classList.toggle('notScroll')
+}
+burger.addEventListener('click', menuBurgerClick); 
